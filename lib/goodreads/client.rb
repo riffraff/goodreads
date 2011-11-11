84987c16a36ef068c93f8d9cac6f68c6141bad72
Gabriele Renzi <rff.rff@gmail.com>
2011-11-11 11:24:29 +0100

    change format, can't pass id as param

--- lib/goodreads/client.rb
+++ lib/goodreads/client.rb
@@ -24,8 +24,7 @@ module Goodreads
     
     # Get review list for user ID
     def review_list(user_id, opts={})
-      opts=opts.merge(:id => user_id)
-      Hashie::Mash.new(request('/review/list', opts))
+      Hashie::Mash.new(request("/review/list/#{user_id}", opts))
     end
 
     # Get book details by Goodreads book ID
